74865e2778cdf73be191208425021604c5c4ebc5
Michael Lewis <mslewis.aero@gmail.com>
2017-04-05 20:27:23 -0600

    stopping work on contact for now.

--- form-handler-nodb.php
+++ form-handler-nodb.php
@@ -13,8 +13,18 @@ $message = $_POST['message'];
 // set up email
 $msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
 $msg = wordwrap($msg,70);
-mail($myemail,"New Form Submission",$msg);
-mail($email,"Thank you for your form submission",$msg);
-echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
+
+if(mail($myemail,"New Form Submission",$msg))
+{
+  if(mail($email,"Thank you for your form submission",$msg))
+  {
+    echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
+  }
+}
+else {
+  echo 'something went wrong';
+}
+
+
 
 ?>
